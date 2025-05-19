1ce663a17a3c8b32bb7d5c2ce2291dd6d721db0f
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2025-05-19 11:24:17 -0300

    Add parallax scrolling changes with scroll observer

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -3,9 +3,24 @@ import Image from 'next/image'
 import logosm from '@/app/public/logo-sm.svg'
 import logolg from '@/app/public/logo-lg.svg'
 import MediumButton from './buttons/MediumButton'
-export default function HomePage() {
+
+import React, { useState, useRef, useCallback, useContext } from 'react'
+import { ScrollContext } from '../utils/scroll-observer'
+const HomePage: React.FC = () => {
+  const refContainer = useRef<HTMLDivElement>(null)
+  const { scrollY } = useContext(ScrollContext)
+
+  let progress = 0
+
+  const { current: elContainer } = refContainer
+  if (elContainer) {
+    progress = Math.min(1, scrollY / elContainer.clientHeight)
+  }
+
   return (
-    <div className="h-screen flex flex-col items-center pt-12 gap-5 md:pt-0 md:flex-row md:flex md:justify-around" id="Home">
+    <div className="h-screen flex flex-col items-center pt-12 gap-5 md:pt-0 md:flex-row md:flex md:justify-around sticky top-0 -z-10" id="Home" ref={refContainer} style={{
+      transform: `translateY(-${progress * 20}vh)`
+    }}>
       <Image src={logosm} alt="Netarise logo" width={650} height={650} className='w-80 h-72' />
 
       <div className='flex flex-col gap-12 mx-6 md:ml-14 md:gap-12 '>
@@ -22,3 +37,5 @@ export default function HomePage() {
     </div>
   )
 }
+
+export default HomePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import Clients from "./components/Clients";
 import Contact from "./components/Contact";
 import Services from "./components/Services";
 import Team from "./components/Team";
-
+import ScrollObserver from "@/app/utils/scroll-observer";
 
 export default function Home() {
   return (
     <>
-      <Clients />
-      <Services />
-      <Team />
-      <Contact />
+      <ScrollObserver>
+        <Clients />
+        <Services />
+        <Team />
+        <Contact />
+      </ScrollObserver>
     </>
   );
 }
